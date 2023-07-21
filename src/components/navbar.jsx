3bde5f7948e8f854de2f90b5673d2cd8338a14e2
jenny-tmg <jennytmg09@gmail.com>
2023-07-21 16:24:34 +0545

    "added search function"

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,39 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import {ShoppingCart} from "phosphor-react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+import { FaSearch } from "react-icons/fa";
 import "./navbar.css";
+import Search from '../pages/search/search';
+
+export const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = () => {
+    
+    fetch(`https://fakestoreapi.com/products?title=${searchTerm}`)
+      .then((res) => res.json())
+      .then((json) => setSearchResults(json));
+  };
+
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+      <Search
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          searchResults={searchResults}
+          setSearchResults={setSearchResults}
+          handleSearch={handleSearch} 
+        />
+    </div>
+  );
+};
 
 
-export const Navbar =() =>{
-    return <div className ="navbar">
-        <div className ="links">
-          <Link to ="/"> Shop </Link>
-          <Link to ="/cart">
-          <ShoppingCart size={32} /> 
-          </Link>
-        </div>
-    </div>;
-}
+
